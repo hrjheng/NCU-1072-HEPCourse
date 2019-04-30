1f63bf668958d9c3a7dd89d6efd93e54957593c7
Hao Ren, Jheng <haorenjheng21@gmail.com>
2019-04-30 09:58:34 +0800

    update

--- GlauberMC/MassiveGen.sh
+++ GlauberMC/MassiveGen.sh
@@ -1,8 +1,15 @@
-nohup python GlauberMC-MassiveGen.py -n Pb_207 -b -1.0 -x 42.0 -N 10000 &> Pb-42.0mb.txt &
-nohup python GlauberMC-MassiveGen.py -n Au_197 -b -1.0 -x 42.0 -N 10000 &> Au-42.0mb.txt &
-nohup python GlauberMC-MassiveGen.py -n Cu_62 -b -1.0 -x 42.0 -N 10000 &> Cu-42.0mb.txt &
-nohup python GlauberMC-MassiveGen.py -n Ni_58 -b -1.0 -x 42.0 -N 10000 &> Ni-42.0mb.txt &
-nohup python GlauberMC-MassiveGen.py -n Ca_40 -b -1.0 -x 42.0 -N 10000 &> Ca-42.0mb.txt &
-nohup python GlauberMC-MassiveGen.py -n S_32 -b -1.0 -x 42.0 -N 10000 &> S-42.0mb.txt &
-nohup python GlauberMC-MassiveGen.py -n Si_28 -b -1.0 -x 42.0 -N 10000 &> Si-42.0mb.txt &
-nohup python GlauberMC-MassiveGen.py -n Pb_207 -b -1.0 -x 72.0 -N 10000 &> Pb-72.0mb.txt &
+nohup ./GlauberMC Pb-Pb -1.0 42.0 1000000 &> Pb-42.0mb.txt &
+nohup ./GlauberMC Au-Au -1.0 42.0 1000000 &> Au-42.0mb.txt &
+nohup ./GlauberMC Cu-Cu -1.0 42.0 1000000 &> Cu-42.0mb.txt &
+nohup ./GlauberMC Ni-Ni -1.0 42.0 1000000 &> Ni-42.0mb.txt &
+nohup ./GlauberMC Ca-Ca -1.0 42.0 1000000 &> Ca-42.0mb.txt &
+nohup ./GlauberMC S-S -1.0 42.0 1000000 &> S-42.0mb.txt &
+nohup ./GlauberMC Si-Si -1.0 42.0 1000000 &> Si-42.0mb.txt &
+
+nohup ./GlauberMC Pb-Pb -1.0 72.0 1000000 &> Pb-72.0mb.txt &
+nohup ./GlauberMC Au-Au -1.0 72.0 1000000 &> Au-72.0mb.txt &
+nohup ./GlauberMC Cu-Cu -1.0 72.0 1000000 &> Cu-72.0mb.txt &
+nohup ./GlauberMC Ni-Ni -1.0 72.0 1000000 &> Ni-72.0mb.txt &
+nohup ./GlauberMC Ca-Ca -1.0 72.0 1000000 &> Ca-72.0mb.txt &
+nohup ./GlauberMC S-S -1.0 72.0 1000000 &> S-72.0mb.txt &
+nohup ./GlauberMC Si-Si -1.0 72.0 1000000 &> Si-72.0mb.txt &
